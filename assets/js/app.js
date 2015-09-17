73b3104186406e69326af9a3aad1a8604de6e9ca
JulesWebb <hello@juleswebb.com>
2015-09-17 16:36:22 -0700

    6th commit pushing dev site to gihub

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@ $(document).foundation();
 
 /* trigger modal by event */
 /* $('#cta').foundation('reveal', 'click'); */
-$('a.reveal-link').trigger('click');
-$('a.close-reveal-modal').trigger('click');
+/* $('a.reveal-link').trigger('click'); */
+/* $('a.close-reveal-modal').trigger('click'); */
 
 
  /* SVG / PNG */
