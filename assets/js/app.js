db1a75ae2f1c26772bb6f6d87a73d535d3e85dbe
JulesWebb <hello@juleswebb.com>
2015-09-17 15:47:02 -0700

    4th commit pushing dev site to gihub

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,30 @@
 $(document).foundation();
 
-    /* trigger modal by event */
-    //$('#cta').foundation('reveal', 'click');
+/* trigger modal by event */
+/* $('#cta').foundation('reveal', 'click'); */
+$('a.reveal-link').trigger('click');
+$('a.close-reveal-modal').trigger('click');
 
 
+ /* SVG / PNG */
+ if (!Modernizr.svg) {
+     $('img[src*="svg"]').attr('src', function () {
+         return $(this).attr('src').replace('.svg', '.png');
+     });
+}
 
+ /*
+ function sideNav() {
+  if ($(window).width() < 769) {
+    $('.off-canvas-wrap').removeClass('move-right');
+    $('.left-off-canvas-toggle').show();
+  } else {
+    $('.off-canvas-wrap').addClass('move-right');
+    $('.left-off-canvas-toggle').hide();
+  }
+}
 
-     /* SVG / PNG */
-     if (!Modernizr.svg) {
-         $('img[src*="svg"]').attr('src', function () {
-             return $(this).attr('src').replace('.svg', '.png');
-         });
-    }
-
-//function sideNav() {
-//  if ($(window).width() < 769) {
-//    $('.off-canvas-wrap').removeClass('move-right');
-//    $('.left-off-canvas-toggle').show();
-//  } else {
-//    $('.off-canvas-wrap').addClass('move-right');
-//    $('.left-off-canvas-toggle').hide();
-//  }
-//}
-//
-//$(window).resize(function() {
-//  sideNav();
-//});
+$(window).resize(function() {
+  sideNav();
+});
+*/
